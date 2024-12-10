e984a66541b160f72cee472d16519346afa612c6
Thomas Noone <tnoone@lcs.on.ca>
2024-12-10 10:10:00 -0500

    Initial Commit

---
+++ SeasonsApp/SeasonsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonsAppApp.swift
+//  SeasonsApp
+//
+//  Created by Thomas Noone on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct SeasonsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
